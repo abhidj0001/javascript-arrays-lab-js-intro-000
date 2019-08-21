20d501074113c709120179807d57ac652283a702
Abhishek Jadhav <abhidj0001@gmail.com>
2019-08-21 12:11:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-const app="I don't do much";
-var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
+const app = "I don't do much."
 
-// Add your functions and code here
-function destructivelyAppendKitten(name) {
-  return kittens.push(name);
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+    return kittens.push(name)
 }
 
-function destructivelyPrependKitten(name) {
-  return kittens.unshift(name);
+function destructivelyPrependKitten(name){
+    return kittens.unshift(name)
 }
 
-function destructivelyRemoveLastKitten(name) {
-  return kittens.pop(name);
+function destructivelyRemoveLastKitten(){
+    return kittens.pop()
 }
 
-function destructivelyRemoveFirstKitten(name) {
-  return kittens.shift(name);
+function destructivelyRemoveFirstKitten(){
+    return kittens.shift()
 }
 
-function appendKitten(name) {
-  return [...kittens,name];
+function appendKitten(name){
+    return [...kittens, name]
 }
 
-function prependKitten(name) {
-  return [name,...kittens];
+function prependKitten(name){
+    return [name, ...kittens]
 }
 
-function removeLastKitten() {
-  return kittens.slice(0,kittens.lenght-1);
+function removeLastKitten(){
+    return kittens.slice(0,kittens.length-1)
 }
 
-function removeFirstKitten() {
-  return kittens.slice(1);
+function removeFirstKitten(){
+    return kittens.slice(1)
 }
